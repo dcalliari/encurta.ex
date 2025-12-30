c1bc0863fa42d8dd47231cc5a5408c95d17030db
daniel calliari <dbcalliari@gmail.com>
2025-12-30 11:45:34 -0300

    fix: update changeset to validate URL format and trim whitespace

--- lib/encurta/links/url.ex
+++ lib/encurta/links/url.ex
@@ -12,9 +12,40 @@ defmodule Encurta.Links.Url do
   @doc false
   def changeset(url, attrs) do
     url
-    |> cast(attrs, [:url, :short])
+    |> cast(attrs, [:url])
     |> validate_required([:url])
-    |> validate_format(:url, ~r/^https?:\/\/.+/)
+    |> update_change(:url, &normalize_url/1)
+    |> validate_length(:url, max: 2048)
+    |> validate_change(:url, &validate_url/2)
     |> unique_constraint(:short)
   end
+
+  defp normalize_url(url) when is_binary(url), do: String.trim(url)
+  defp normalize_url(url), do: url
+
+  defp validate_url(:url, url) when is_binary(url) do
+    cond do
+      url == "" ->
+        [url: "can't be blank"]
+
+      String.contains?(url, ["\r", "\n", "\t", " "]) ->
+        [url: "must not contain whitespace"]
+
+      true ->
+        uri = URI.parse(url)
+
+        cond do
+          uri.scheme not in ["http", "https"] ->
+            [url: "must start with http:// or https://"]
+
+          is_nil(uri.host) or uri.host == "" ->
+            [url: "must include a valid host"]
+
+          true ->
+            []
+        end
+    end
+  end
+
+  defp validate_url(:url, _), do: [url: "is invalid"]
 end
